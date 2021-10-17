11cb7593e472509ebcab1c1435081056295ae518
simisimon <Bastisimon95@googlemail.com>
2021-10-17 12:30:43 +0200

    Add maven and docker plugin

---
+++ src/cfgnet/plugins/concept/docker_plugin.py
@@ -0,0 +1,203 @@
+# This file is part of the CfgNet network module.
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import os
+import re
+
+from typing import List, Any
+
+from cfgnet.network.nodes import ArtifactNode, OptionNode, ValueNode
+from cfgnet.plugins.plugin import Plugin
+
+
+class Command:
+    cmd = None
+    start_line = None
+    value: List[Any] = []
+
+    def __init__(self, cmd, start_line, value):
+        self.cmd = cmd
+        self.start_line = start_line
+        self.value = value
+
+
+def parse_string(content):
+    data = []
+    line_no = 0
+    cmd = None
+    for line in content.splitlines():
+        line_no += 1
+        line = line.strip()
+        if line.startswith("#") or len(line) == 0:
+            continue  # TODO: support parser directives
+
+        # treat quote-enclosed parts by escaping spaces and commas within
+        quotes = re.split(r"((?<!\\)[\"\'].*?(?<!\\)[\"\'])", line)
+        quotes = [
+            re.sub(r"([ ,])", r"\\\1", p)[1:-1]
+            if re.match(r"[\"\']", p)
+            else p
+            for p in quotes
+        ]
+        line = "".join(quotes)
+
+        parts = [p.replace("\\ ", " ") for p in re.split(r"(?<!\\) ", line)]
+        if cmd is None:
+            instruction = parts[0].lower()
+            if len(instruction) == 0:
+                continue
+            cmd = Command(cmd=instruction, start_line=line_no, value=parts[1:])
+        else:
+            cmd.value += parts
+
+        if cmd is not None:
+            # no \ at eol i.e. end of command
+            if not re.match(r".*\\\s*$", line):
+                if cmd.cmd == "env":
+                    cmd.value = "=".join(cmd.value).split("=")
+                elif cmd.cmd in ["cmd", "entrypoint"]:
+                    without_brackets = " ".join(cmd.value)[1:-1]
+                    params = without_brackets.replace(" ", "").split(",")
+                    cmd.value = [" ".join(params)]
+                data.append(cmd)
+                cmd = None
+            else:
+                cmd.value = cmd.value[:-1]
+    return data
+
+
+def parse_file(abs_file_path):
+    with open(abs_file_path, "r", encoding="utf-8") as file:
+        return parse_string(file.read())
+
+
+class DockerPlugin(Plugin):
+    expose_command = re.compile(
+        r"(?P<port>[0-9]{2,4})(\/)(?P<protocol>(tcp|udp))"
+    )
+
+    def __init__(self):
+        super().__init__("docker")
+
+    def _parse_config_file(self, abs_file_path, rel_file_path, root):
+        file_name = os.path.basename(abs_file_path)
+
+        artifact = ArtifactNode(file_name, abs_file_path, rel_file_path, root)
+
+        data = parse_file(abs_file_path)
+        # files that are destinations in `ADD` and `COPY`
+        destination_files = []
+
+        for cmd in data:
+            option = OptionNode(cmd.cmd, cmd.start_line)
+            artifact.add_child(option)
+
+            if cmd.cmd == "from":
+                if len(cmd.value) == 3:
+                    image = ValueNode(cmd.value[0])
+                    name = ValueNode(cmd.value[2])
+                    option_image = OptionNode("image", cmd.start_line)
+                    option_name = OptionNode("name", cmd.start_line)
+                    option.add_child(option_image)
+                    option.add_child(option_name)
+                    option_image.add_child(image)
+                    option_name.add_child(name)
+                else:
+                    option.add_child(ValueNode(cmd.value[0]))
+
+            if cmd.cmd == "env":
+                if len(cmd.value) % 2 == 0:
+                    for i in range(0, len(cmd.value), 2):
+                        env_var_option = OptionNode(
+                            cmd.value[i], cmd.start_line
+                        )
+                        option.add_child(env_var_option)
+
+                        env_var_option.add_child(ValueNode(cmd.value[i + 1]))
+            elif cmd.cmd in ["cmd", "entrypoint"]:
+                exec_command = " ".join(cmd.value)
+                exec_command_node = OptionNode(
+                    "exec_command", location=option.location
+                )
+                option.add_child(exec_command_node)
+                exec_command_node.add_child(ValueNode(exec_command))
+
+                parameters = cmd.value[0].split(" ")
+                self._add_params(option, parameters)
+
+            elif cmd.cmd in ["add", "copy"]:
+                src = OptionNode("src", cmd.start_line)
+                option.add_child(src)
+                # remove leading `./` or `/`
+                source_file = re.sub(r"^(\.)?/", "", cmd.value[-2])
+                src.add_child(ValueNode(source_file))
+                dest = OptionNode("dest", cmd.start_line)
+                option.add_child(dest)
+                destination_file = cmd.value[-1]
+                dest.add_child(ValueNode(destination_file))
+                destination_files.append(destination_file)
+                if len(cmd.value) > 2:  # handle flags
+                    flag_re = re.compile(r"--(chown|from)=([^ ]+)")
+                    for part in cmd.value:
+                        match = flag_re.match(part)
+                        if match:
+                            flag_key = match.group(1)
+                            flag_value = match.group(2)
+                            flag_node = OptionNode(flag_key, cmd.start_line)
+                            option.add_child(flag_node)
+                            flag_node.add_child(ValueNode(flag_value))
+
+            elif cmd.cmd == "expose":
+                for value in cmd.value:
+                    self._parse_expose(option, value)
+
+            if not option.children:
+                artifact.children.remove(option)
+
+        return artifact
+
+    def is_responsible(self, abs_file_path):
+        file_name = os.path.basename(abs_file_path)
+
+        if file_name == "Dockerfile":
+            return True
+
+        return False
+
+    def _parse_expose(self, option, value):
+        match = self.expose_command.fullmatch(value)
+        if match:
+            port = ValueNode(match.group("port"))
+            protocol = ValueNode(match.group("protocol"))
+            option_port = OptionNode("port", option.location)
+            option_protocol = OptionNode("protocol", option.location)
+            option.add_child(option_port)
+            option.add_child(option_protocol)
+            option_port.add_child(port)
+            option_protocol.add_child(protocol)
+        else:
+            option.add_child(ValueNode(value))
+
+    @staticmethod
+    def _add_params(option, parameters):
+        param_counter = 0
+        for param in parameters:
+            option_param = OptionNode(
+                "param" + str(param_counter), option.location
+            )
+            option.add_child(option_param)
+            value = re.sub(r"^(\.)?/", "", param)
+            option_param.add_child(ValueNode(value))
+            param_counter += 1
---
+++ src/cfgnet/plugins/concept/maven_plugin.py
@@ -0,0 +1,262 @@
+# This file is part of the CfgNet module.
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import logging
+import os
+
+from lxml import etree as ET
+
+from cfgnet.network.nodes import ArtifactNode, Node, OptionNode, ValueNode
+from cfgnet.plugins.plugin import Plugin
+
+TAGS_CONTAINING_LISTS = {
+    "goal",
+    "exclude",
+    "include",
+    "arg",
+    "pattern",
+    "class",
+    "package",
+    "ignoredResourcePattern",
+}
+
+
+class MavenPlugin(Plugin):
+    def __init__(self):
+        super().__init__("maven")
+
+    def _parse_config_file(self, abs_file_path, rel_file_path, root):
+        file_name = os.path.basename(abs_file_path)
+
+        artifact = ArtifactNode(file_name, abs_file_path, rel_file_path, root)
+
+        try:
+            maven_tree = ET.parse(abs_file_path)
+            tree_root = maven_tree.getroot()
+
+            # Remove namespace prefixes
+            for elem in tree_root.getiterator():
+                if elem.tag is not ET.Comment:
+                    elem.tag = ET.QName(elem).localname
+            # Remove unused namespace declarations
+            ET.cleanup_namespaces(tree_root)
+
+            option_root = OptionNode(tree_root.tag, tree_root.sourceline)
+            artifact.add_child(option_root)
+            for child in tree_root:
+                if child.tag is not ET.Comment:
+                    self.parse_tree(child, parent_node=option_root)
+
+        except ET.Error as error:
+            logging.warning(
+                'Failed to parse xml file "%s" due to %s', rel_file_path, error
+            )
+
+        self._add_executable_name(artifact)
+
+        return artifact
+
+    def is_responsible(self, abs_file_path):
+        file_name = os.path.basename(abs_file_path)
+
+        if file_name == "pom.xml":
+            return True
+
+        return False
+
+    def parse_tree(self, subtree_root, parent_node: Node):
+        name = self._make_name(subtree_root)
+        if name:
+            current_node = OptionNode(name, subtree_root.sourceline)
+            parent_node.add_child(current_node)
+
+            self._add_attribs(subtree_root, current_node)
+
+            text = subtree_root.text
+            if text:
+                text = text.strip()
+                if text:
+                    value_node = ValueNode(text)
+                    current_node.add_child(value_node)
+
+            for child in subtree_root:
+                if child.tag is not ET.Comment:
+                    self.parse_tree(child, current_node)
+
+            # remove option nodes without children
+            if not current_node.children:
+                parent_node.children.remove(current_node)
+
+    @staticmethod
+    def _add_attribs(subtree_root, current_node):
+        current_attribs = subtree_root.attrib
+        for key in current_attribs:
+            option_node = OptionNode(key, subtree_root.sourceline)
+            current_node.add_child(option_node)
+
+            value = current_attribs[key]
+            value_node = ValueNode(value)
+            option_node.add_child(value_node)
+
+    # pylint: disable=too-many-return-statements
+    @staticmethod
+    def _make_name(current_item):
+        """
+        Construct a name for an option node to avoid ambiguous option nodes.
+
+        :param current_item: lxml etree element that should be inserted as an option node
+        :return: constructed name
+        """
+        name_element = current_item.find("id")
+        if name_element is not None:
+            return current_item.tag + "_" + name_element.text
+
+        name_element = current_item.find("groupId")
+        if name_element is not None:
+            item_artifact_id = current_item.find("artifactId")
+            if item_artifact_id is not None:
+                return (
+                    current_item.tag
+                    + "_"
+                    + name_element.text
+                    + "/"
+                    + item_artifact_id.text
+                )
+
+            return current_item.tag + "_" + name_element.text
+
+        item_artifact_id = current_item.find("artifactId")
+        if item_artifact_id is not None:
+            return current_item.tag + "_" + item_artifact_id.text
+
+        name_element = current_item.find("name")
+        if name_element is not None:
+            return current_item.tag + "_" + name_element.text
+
+        name_element = current_item.find("key")
+        if name_element is not None:
+            return current_item.tag + "_" + name_element.text
+
+        if current_item.tag == "notifier":
+            name_element = current_item.find("type")
+            if name_element is not None:
+                return current_item.tag + "_" + name_element.text
+
+        if current_item.tag in TAGS_CONTAINING_LISTS:
+            if current_item.text is not None:
+                return current_item.tag + "_" + current_item.text
+
+        return current_item.tag
+
+    def _add_executable_name(self, artifact):
+        try:
+            option_nodes = artifact.get_nodes(node_type=OptionNode)
+            project_option = next(
+                filter(lambda node: node.name == "project", option_nodes)
+            )
+            project_option_children = project_option.children
+            artifactid_node = next(
+                filter(
+                    lambda node: node.name == "artifactId",
+                    project_option_children,
+                )
+            )
+            artifactid = artifactid_node.children[0].name
+            artifactid_location = str(artifactid_node.location)
+
+            version, version_location = self._get_version_for_executable_name(
+                project_option
+            )
+            (
+                packaging,
+                packaging_location,
+            ) = self._get_packaging_for_executable_name(project_option)
+
+            location = self.merge_executable_name_locations(
+                artifactid_location, version_location, packaging_location
+            )
+
+            executable_name_prefix = "target/"
+
+            executable_name = f"{artifactid}{version}.{packaging}"
+
+            option_node = OptionNode("ExecutableName", location)
+            value_node = ValueNode(executable_name_prefix + executable_name)
+
+            artifact.add_child(option_node)
+            option_node.add_child(value_node)
+
+            if version != "":
+                executable_name_no_version = f"{artifactid}.{version}"
+                option_node_no_version = OptionNode(
+                    "ExecutableNameNoVersion", location
+                )
+                artifact.add_child(option_node_no_version)
+                option_node_no_version.add_child(
+                    ValueNode(
+                        executable_name_prefix + executable_name_no_version
+                    )
+                )
+
+        except StopIteration:
+            logging.warning(
+                "Failed to add executable name to artifact %s",
+                artifact.rel_file_path,
+            )
+
+    @staticmethod
+    def merge_executable_name_locations(
+        artifactid_location, version_location, packaging_location
+    ):
+        location = artifactid_location
+        if version_location and packaging_location:
+            location += ", " + version_location + ", and " + packaging_location
+        elif version_location and not packaging_location:
+            location += " and " + version_location
+        elif not version_location and packaging_location:
+            location += " and " + packaging_location
+        return location
+
+    @staticmethod
+    def _get_version_for_executable_name(project_option_node):
+        try:
+            version_node = next(
+                filter(
+                    lambda node: node.name == "version",
+                    project_option_node.children,
+                )
+            )
+            return (
+                "-" + version_node.children[0].name,
+                str(version_node.location),
+            )
+        except StopIteration:
+            return "", None
+
+    @staticmethod
+    def _get_packaging_for_executable_name(project_option_node):
+        try:
+            packaging_node = next(
+                filter(
+                    lambda node: node.name == "packaging",
+                    project_option_node.children,
+                )
+            )
+            return (
+                packaging_node.children[0].name,
+                str(packaging_node.location),
+            )
+        except StopIteration:
+            return "jar", None

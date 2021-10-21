6b9a323b23c8d7c5a67101c31613cd7d9e441213
Sebastian Simon <ssimon@informatik.uni-leipzig.de>
2021-10-21 14:29:12 +0200

    Add conflict detector test and increase python version

---
+++ tests/cfgnet/conflicts/test_conflict_detector.py
@@ -0,0 +1,60 @@
+# This file is part of the CfgNet module.
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import pytest
+
+from cfgnet.conflicts.conflict import (
+    MissingOptionConflict,
+    ModifiedOptionConflict,
+)
+from cfgnet.conflicts.conflict_detector import ConflictDetector
+from cfgnet.network.network import Network
+from tests.utility.temporary_repository import TemporaryRepository
+
+
+@pytest.fixture(name="get_networks")
+def get_networks_():
+    """Return the conflicts and the updated simple git repository."""
+    repo = TemporaryRepository(
+        "tests/test_repos/maven_docker/0001-Add-Docker-and-maven-file.patch"
+    )
+    ref_network = Network.init_network(project_root=repo.root)
+
+    repo.apply_patch(
+        "tests/test_repos/maven_docker/0002-Provoke-two-conflicts.patch"
+    )
+    new_network = Network.init_network(project_root=repo.root)
+
+    return ref_network, new_network
+
+
+def test_detect_conflicts(get_networks):
+    ref_network = get_networks[0]
+    new_network = get_networks[1]
+
+    conflicts = ConflictDetector.detect(
+        ref_network=ref_network, new_network=new_network
+    )
+
+    modified_option_conflict = list(
+        filter(lambda x: isinstance(x, ModifiedOptionConflict), conflicts)
+    )
+    missing_option_conflict = list(
+        filter(lambda x: isinstance(x, MissingOptionConflict), conflicts)
+    )
+
+    assert len(conflicts) == 2
+    assert len(modified_option_conflict) == 1
+    assert len(missing_option_conflict) == 1

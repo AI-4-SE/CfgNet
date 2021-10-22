414adc1a858876e3c7a192d9d2fec401e15e33ef
Jakob Ruckel <jakob@ruckel.de>
2021-10-22 19:56:55 +0200

    Launcher: Add remaining commands

--- src/cfgnet/launcher.py
+++ src/cfgnet/launcher.py
@@ -1,8 +1,11 @@
+import sys
+from typing import List, Any
+import logging
 import click
 
-from cfgnet.network.network import Network
-from cfgnet.launcher_configuration import LauncherConfiguration
-
+from cfgnet.launcher_configuration import (
+    LauncherConfiguration,
+)
 
 pass_launcher_configuration = click.make_pass_decorator(
     LauncherConfiguration, ensure=True
@@ -13,7 +16,7 @@ pass_launcher_configuration = click.make_pass_decorator(
 @click.option(
     "-v", "--verbose", help="Log everything to console.", is_flag=True
 )
-@click.argument("project_root")
+@click.argument("project_root")  # TODO type directory
 @pass_launcher_configuration
 def main(cfg: LauncherConfiguration, project_root: str, verbose: bool):
     cfg.project_root = project_root
@@ -33,9 +36,78 @@ def init(
     cfg.enable_static_blacklist = enable_static_blacklist
     cfg.enable_dynamic_blacklist = enable_dynamic_blacklist
 
-    Network.init_network(cfg.project_root)
+    logging.info("Initializing network for '%s'.", cfg.project_root)
 
-    return True
+    # TODO Network.init_network(cfg)
+
+
+@main.command()
+@pass_launcher_configuration
+def validate(cfg: LauncherConfiguration):
+    logging.info("Validating network for '%s'.", cfg.project_root)
+
+    # TODO network: Network = Network.load_network(cfg)
+
+    # TODO Change Any to Conflict once conflict class is added
+    # TODO replace empty list with call to `network.validate()`
+    conflicts: List[Any] = []
+
+    if len(conflicts) == 0:
+        logging.info("No conflicts detected. Updated reference network.")
+        # TODO network.save(cfg)
+        return
+
+    logging.error(
+        "Detected %d configuration conflicts.",
+        sum([conflict.count() for conflict in conflicts]),
+    )
+
+    for conflict in conflicts:
+        print(conflict)
+
+    sys.exit(1)
+
+
+@main.command()
+@click.option("-b", "--enable-static-blacklist", is_flag=True)
+@click.option("-d", "--enable-dynamic-blacklist", is_flag=True)
+@pass_launcher_configuration
+def analyze(
+    cfg: LauncherConfiguration,
+    enable_static_blacklist: bool,
+    enable_dynamic_blacklist: bool,
+):
+    """Run self-evaluating analysis of commit history of any repository."""
+    cfg.enable_static_blacklist = enable_static_blacklist
+    cfg.enable_dynamic_blacklist = enable_dynamic_blacklist
+
+    logging.info("Analyzing network for '%s'.", cfg.project_root)
+
+    # TODO Network.analyze(cfg)
+
+
+@main.command()
+@click.option("-o", "--output", required=True)  # TODO type
+@click.option("-f", "--format", "export_format", required=True)  # TODO type
+@click.option("-u", "--include-unlinked", is_flag=True, help="TODO")
+@click.option("--visualize-dot", is_flag=True, help="TODO")
+@pass_launcher_configuration
+def export(
+    cfg: LauncherConfiguration,
+    output: str,
+    export_format: str,
+    include_unlinked: bool,
+    visualize_dot: bool,
+):
+    cfg.export_output = output
+    cfg.export_format = export_format
+    cfg.export_include_unlinked = include_unlinked
+    cfg.export_visualize_dot = visualize_dot
+
+    logging.info("Exporting network for '%s'.", cfg.project_root)
+
+    # TODO network = Network.load_network(cfg)
+    # TODO network.export(cfg)
 
 
 if __name__ == "__main__":
--- src/cfgnet/launcher_configuration.py
+++ src/cfgnet/launcher_configuration.py
@@ -4,7 +4,22 @@ from typing import Optional
 
 @dataclass()
 class LauncherConfiguration:
+    # global, for all commands
     project_root: str = "."
     verbose: bool = False
-    enable_static_blacklist: Optional[bool] = None
-    enable_dynamic_blacklist: Optional[bool] = None
+
+    # initialize, analyze
+    enable_static_blacklist: bool = False
+    enable_dynamic_blacklist: bool = False
+
+    # analyze
+    check_network_integrity: bool = False
+
+    # export
+    export_output: Optional[str] = None
+    export_format: Optional[str] = None  # TODO: Create format enum in exporter
+    export_include_unlinked: bool = False
+    export_visualize_dot: bool = False
+
+
+DEFAULT_LAUNCHER_CFG = LauncherConfiguration()  # TODO do we need this?

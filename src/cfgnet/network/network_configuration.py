b4bc9c280900b251c210f73817b374d942cf34fd
Jakob Ruckel <jakob@ruckel.de>
2021-11-03 14:20:35 +0100

    NetworkConfiguration: Add default data directory

--- src/cfgnet/network/network_configuration.py
+++ src/cfgnet/network/network_configuration.py
@@ -7,3 +7,5 @@ class NetworkConfiguration:
     project_root_abs: str
     enable_static_blacklist: bool
     enable_dynamic_blacklist: bool
+    # Path to CfgNet data directory relative to project_root
+    cfgnet_path_rel: str = ".cfgnet"

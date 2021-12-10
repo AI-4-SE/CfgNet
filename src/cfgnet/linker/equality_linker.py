30fc2e7e02a08bf4eb1efa0a8f0b6cf3084df38e
Sebastian Simon <ssimon@informatik.uni-leipzig.de>
2021-12-10 15:49:39 +0100

    Revise base and equality linker and avoid links within the same maven file

--- src/cfgnet/linker/equality_linker.py
+++ src/cfgnet/linker/equality_linker.py
@@ -21,6 +21,26 @@ from cfgnet.linker.linker import Linker
 class EqualityLinker(Linker):
     """Equality-based Linker."""
 
+    def create_links(self) -> None:
+        self.target_nodes = self._find_target_nodes()
+
+        for node in self.target_nodes:
+            if not node.name:
+                return
+
+            # discard words from static blacklist
+            if self.network:
+                if self.network.cfg.enable_static_blacklist:
+                    if node.name in self.static_blacklist.values:
+                        return
+
+            # find all matches with the given linker criterion
+            matches = self._find_matches(node)
+
+            # add link for all matches
+            for match in matches:
+                self._add_link(node, match)
+
     def _find_target_nodes(self):
         return [
             node
@@ -29,8 +49,25 @@ class EqualityLinker(Linker):
         ]
 
     def _find_matches(self, node: ValueNode) -> List[ValueNode]:
+
+        target_nodes = self._filter_target_nodes(node)
+
         return [
             value_node
-            for value_node in self.target_nodes
+            for value_node in target_nodes
             if value_node.name == node.name and node is not value_node
         ]
+
+    def _filter_target_nodes(self, node) -> List[ValueNode]:
+        """Filter target nodes to avoid links within the same maven file."""
+        artifact_name = node.id.split("::::")[1]
+
+        if "pom.xml" in artifact_name:
+            return list(
+                filter(
+                    lambda node: artifact_name not in node.id.split("::::")[1],
+                    self.target_nodes,
+                )
+            )
+
+        return self.target_nodes

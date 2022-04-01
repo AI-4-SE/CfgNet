2d28f43f4153c087863176ba0a792902a4751a6f
simisimon <Bastisimon95@googlemail.com>
2022-04-01 16:19:52 +0200

    Add cypress plugin and corresponding tests

---
+++ src/cfgnet/plugins/concept/cypress_plugin.py
@@ -0,0 +1,69 @@
+# This file is part of the CfgNet module.
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program.  If not, see <https://www.gnu.org/licenses/>.
+from cfgnet.plugins.file_type.json_plugin import JsonPlugin
+from cfgnet.config_types.config_types import ConfigType
+
+
+class CypressPlugin(JsonPlugin):
+    def __init__(self):
+        super().__init__("cypress")
+        self.excluded_keys = []
+
+    def is_responsible(self, abs_file_path: str) -> bool:
+        if abs_file_path.endswith("cypress.json"):
+            return True
+        return False
+
+    # pylint: disable=too-many-return-statements
+    def get_config_type(self, option_name: str) -> ConfigType:
+        """
+        Find config type based on option name.
+
+        :param option_name: name of option
+        :return: config type
+        """
+        if option_name in ("baseUrl", "backendUrl"):
+            return ConfigType.URL
+        if option_name == "env":
+            return ConfigType.ENVIRONMENT
+        if option_name == "port":
+            return ConfigType.PORT
+        if option_name in (
+            "includeShadowDom",
+            "watchForFileChanges",
+            "screenshotOnRunFailure",
+            "trashAssetsBeforeRuns",
+            "trashAssetsBeforeRuns",
+            "video",
+            "videoUploadOnPasses",
+            "trashAssetsBeforeRuns",
+            "chromeWebSecurity",
+            "waitForAnimations",
+        ):
+            return ConfigType.BOOLEAN
+        if any(x in option_name for x in ["Folder", "File", "Files"]):
+            return ConfigType.PATH
+        if option_name == "nodeVersion":
+            return ConfigType.VERSION_NUMBER
+        if option_name in (
+            "defaultCommandTimeout",
+            "execTimeout",
+            "taskTimeout",
+            "pageLoadTimeout",
+            "requestTimeout",
+            "responseTimeout",
+        ):
+            return ConfigType.TIME
+        return ConfigType.UNKNOWN

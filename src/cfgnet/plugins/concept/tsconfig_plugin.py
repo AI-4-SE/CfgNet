57b082309b375e72051cff96948409aa74a4c1a2
simisimon <Bastisimon95@googlemail.com>
2022-04-02 15:01:25 +0200

    Add tsconfig plugin and corresponding tests

---
+++ src/cfgnet/plugins/concept/tsconfig_plugin.py
@@ -0,0 +1,55 @@
+# This file is part of the CfgNet module.
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program.  If not, see <https://www.gnu.org/licenses/>.
+from cfgnet.plugins.file_type.json_plugin import JsonPlugin
+from cfgnet.config_types.config_types import ConfigType
+
+
+class TsconfigPlugin(JsonPlugin):
+    def __init__(self):
+        super().__init__("tsconfig")
+        self.excluded_keys = []
+
+    def is_responsible(self, abs_file_path: str) -> bool:
+        if abs_file_path.endswith("tsconfig.json"):
+            return True
+        return False
+
+    # pylint: disable=too-many-return-statements
+    def get_config_type(self, option_name: str) -> ConfigType:
+        """
+        Find config type based on option name.
+
+        :param option_name: name of option
+        :return: config type
+        """
+        if option_name in (
+            "files",
+            "outFile",
+            "include",
+            "exclude",
+            "extends",
+        ):
+            return ConfigType.PATH
+        if any(x in option_name for x in ["Dir", "File"]):
+            return ConfigType.PATH
+        if option_name in (
+            "noImplicitAny",
+            "removeComments",
+            "preserveConstEnums",
+            "sourceMap",
+            "allowJs",
+        ):
+            return ConfigType.BOOLEAN
+        return ConfigType.UNKNOWN

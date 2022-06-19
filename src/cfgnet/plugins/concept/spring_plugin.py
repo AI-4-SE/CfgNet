36ca8261df02cf4184b33d380605ebd9c9de368e
simisimon <Bastisimon95@googlemail.com>
2022-06-19 11:19:20 +0200

    Add spring plugin and cover spring core properties

---
+++ src/cfgnet/plugins/concept/spring_plugin.py
@@ -0,0 +1,367 @@
+# This file is part of the CfgNet module.
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program.  If not, see <https://www.gnu.org/licenses/>.
+import configparser
+import logging
+import re
+from typing import Optional, Dict
+import flatdict
+import yaml
+from cfgnet.network.nodes import (
+    ProjectNode,
+    ArtifactNode,
+    OptionNode,
+    ValueNode,
+)
+from cfgnet.plugins.plugin import Plugin
+from cfgnet.config_types.config_types import ConfigType
+
+
+class SpringPlugin(Plugin):
+    application_yaml_regex = re.compile(r"application(.(dev|prod)+)?.yml")
+    bootstrap_yaml_regex = re.compile(r"bootstrap(.(dev|prod)+)?.yml")
+    application_properties_regex = re.compile(
+        r"application(.(dev|prod)+)?.properties"
+    )
+    bootstrap_properties_regex = re.compile(
+        r"bootstrap(.(dev|prod)+)?.properties"
+    )
+
+    def __init__(self):
+        super().__init__("spring")
+
+    def is_responsible(self, abs_file_path):
+        if self.application_yaml_regex.search(
+            abs_file_path
+        ) or self.bootstrap_yaml_regex.search(abs_file_path):
+            return True
+        if self.application_properties_regex.search(
+            abs_file_path
+        ) or self.bootstrap_properties_regex.search(abs_file_path):
+            return True
+        return False
+
+    def _parse_config_file(
+        self,
+        abs_file_path: str,
+        rel_file_path: str,
+        root: Optional[ProjectNode],
+    ) -> ArtifactNode:
+        if abs_file_path.endswith(".yml"):
+            artifact = self._parse_yml_file(
+                abs_file_path=abs_file_path,
+                rel_file_path=rel_file_path,
+                root=root,
+            )
+        if abs_file_path.endswith(".properties"):
+            artifact = self._parse_properties_file(
+                abs_file_path=abs_file_path,
+                rel_file_path=rel_file_path,
+                root=root,
+            )
+
+        return artifact
+
+    def _parse_properties_file(
+        self,
+        abs_file_path: str,
+        rel_file_path: str,
+        root: Optional[ProjectNode],
+    ) -> ArtifactNode:
+        """Parse spring properties files."""
+        artifact = ArtifactNode(
+            file_path=abs_file_path,
+            rel_file_path=rel_file_path,
+            concept_name=self.concept_name,
+            project_root=root,
+        )
+
+        with open(abs_file_path, "r", encoding="utf-8") as file:
+            line_dict = {}
+            lineno = 1
+            for line in file:
+                line = line.strip()
+                if len(line) > 0:
+                    line_dict[line] = lineno
+                lineno += 1
+
+        with open(abs_file_path, "r", encoding="utf-8") as config_file:
+            file_content = config_file.read()
+
+        try:
+            dummy_section = "[dummy_section]\n"
+            config = configparser.ConfigParser(
+                interpolation=None, allow_no_value=True
+            )
+            config.read_string(dummy_section + file_content)
+        except (AttributeError, configparser.Error) as error:
+            logging.warning(
+                'Failed to parse ini file "%s"'
+                '" with configparser due to "%s"',
+                rel_file_path,
+                str(error),
+            )
+            return artifact
+
+        for section_name in config:
+            if section_name == "DEFAULT":
+                continue
+
+            section = config[section_name]
+
+            if len(section.keys()) == 0:
+                continue
+
+            if section_name == "dummy_section":
+                parent = artifact
+
+            for option in section.keys():
+                config_type = self.get_config_type(option_name=option)
+                option_node = OptionNode(
+                    name=option,
+                    location=self.get_line_number(
+                        option_name=option, line_dict=line_dict
+                    ),
+                    config_type=config_type,
+                )
+                parent.add_child(option_node)
+
+                value = section[option]
+                if value:
+                    while value.startswith("\n"):
+                        value = value[1:]
+
+                    value = re.sub(r"\\\n\s*", "", value)
+
+                    value_node = ValueNode(value)
+                    option_node.add_child(value_node)
+
+                else:
+                    logging.warning('Empty value in file "%s"', rel_file_path)
+                    parent.children.remove(option_node)
+
+        return artifact
+
+    def get_line_number(self, option_name: str, line_dict: Dict) -> str:
+        """
+        Get line number from line dictionary.
+
+        :param option_name: option name in line
+        :param line_dict: dictionary of lines
+        :return: line number as string
+        """
+        for line in line_dict.keys():
+            if option_name in line:
+                lineno = line_dict[line]
+                del line_dict[line]
+                return str(lineno)
+        return "Unknown"
+
+    # pylint: disable=broad-except
+    def _parse_yml_file(
+        self,
+        abs_file_path: str,
+        rel_file_path: str,
+        root: Optional[ProjectNode],
+    ) -> ArtifactNode:
+        """Parse spring yml files."""
+        artifact = ArtifactNode(
+            file_path=abs_file_path,
+            rel_file_path=rel_file_path,
+            concept_name=self.concept_name,
+            project_root=root,
+        )
+
+        with open(abs_file_path, "r", encoding="utf-8") as file:
+            line_number_dict = {}
+            lineno = 1
+            for line in file:
+                line = line.strip()
+                if len(line) > 0:
+                    line_number_dict[line] = lineno
+                lineno += 1
+
+        try:
+            with open(abs_file_path, encoding="utf-8") as yaml_file:
+                parsed_yaml = yaml.load(yaml_file, Loader=yaml.FullLoader)
+                yaml_dict = flatdict.FlatDict(parsed_yaml, ".")
+                if len(yaml_dict) != 0:
+                    for key, values in yaml_dict.items():
+                        if isinstance(values, list):
+                            yaml_dict[key] = ", ".join(values)
+        except Exception as error:
+            logging.warning(
+                'Failed to parse yml file "%s"'
+                '" with spring parser due to "%s"',
+                rel_file_path,
+                str(error),
+            )
+            return artifact
+
+        for key, value in yaml_dict.items():
+            line_number = self.get_line_number(
+                option_name=key.split(".")[-1], line_dict=line_number_dict
+            )
+            config_type = self.get_config_type(key)
+            option = OptionNode(
+                name=key, location=line_number, config_type=config_type
+            )
+            artifact.add_child(option)
+            value = ValueNode(name=value)
+            option.add_child(value)
+
+        return artifact
+
+    # pylint: disable=too-many-return-statements
+    @staticmethod
+    def get_config_type(option_name: str) -> ConfigType:
+        """
+        Get config type based on the option name.
+
+        :param name: option name
+        :return: ConfigType
+        """
+        if any(
+            option_name.endswith(x)
+            for x in [
+                ".show-sql",
+                ".cache",
+                ".trace",
+                ".await-termination",
+                ".pool.allow-core-thread-timeout",
+                ".wait-for-jobs-to-complete-on-shutdown",
+                ".auto-startup",
+                ".use-code-as-default-message",
+                ".fallback-to-system-locale",
+                ".always-use-message-format",
+                ".register-shutdown-hook",
+                ".log-startup-info",
+                ".lazy-initialization",
+                ".allow-circular-references",
+                ".allow-bean-definition-overriding",
+                ".unique-names",
+                ".use-legacy-processing",
+                ".log-request-details",
+                ".ignore",
+                ".image.invert",
+                "debug",
+                ".clean-history-on-start",
+                ".register-shutdown-hook",
+                ".aop.auto",
+                ".proxy-target-class",
+                ".enabled",
+            ]
+        ):
+            return ConfigType.BOOLEAN
+
+        if any(
+            option_name.endswith(x)
+            for x in [
+                "size",
+                ".max-file-size",
+                ".total-size-cap",
+                ".image.width",
+                ".max-in-memory-size",
+                ".image.height",
+                ".pool.size",
+            ]
+        ):
+            return ConfigType.SIZE
+
+        if any(
+            option_name.endswith(x)
+            for x in [
+                ".max-history",
+                ".image.bitdepth",
+                ".image.margin",
+                ".core-size",
+                ".pool.max-size",
+            ]
+        ):
+            return ConfigType.NUMBER
+
+        if any(
+            option_name.endswith(x)
+            for x in [".pattern.dateformat", ".pattern.file", ".pattern.level"]
+        ):
+            return ConfigType.PATTERN
+
+        if any(
+            option_name.endswith(x)
+            for x in [
+                "platform",
+                "-name",
+                ".database",
+                ".authentication-database",
+                ".provider",
+                ".scheduler-name",
+                ".jmx-name",
+                ".name",
+                ".jmx.server",
+                ".active",
+                ".basenames",
+                ".profiles.default",
+            ]
+        ):
+            return ConfigType.NAME
+
+        if any(
+            option_name.endswith(x)
+            for x in [
+                ".location",
+                ".file",
+                ".jdbc.schema",
+                ".config",
+                ".path",
+                ".image",
+            ]
+        ):
+            return ConfigType.PATH
+
+        if any(option_name.endswith(x) for x in [".image.pixelmode"]):
+            return ConfigType.MODE
+
+        if any(
+            option_name.endswith(x)
+            for x in [".default-domain", ".host", ".uri", "url"]
+        ):
+            return ConfigType.URL
+
+        if any(
+            option_name.endswith(x)
+            for x in [
+                ".timeout-per-shutdown-phase",
+                ".startup-delay",
+                "pool.keep-alive",
+                ".await-termination-period",
+            ]
+        ):
+            return ConfigType.TIME
+
+        if any(option_name.endswith(x) for x in [".port"]):
+            return ConfigType.PORT
+
+        if any(option_name.endswith(x) for x in [".username"]):
+            return ConfigType.USERNAME
+
+        if any(option_name.endswith(x) for x in [".password"]):
+            return ConfigType.PASSWORD
+
+        if any(option_name.endswith(x) for x in [".protocol"]):
+            return ConfigType.PROTOCOL
+
+        if any(option_name.endswith(x) for x in [".mail"]):
+            return ConfigType.EMAIL
+
+        return ConfigType.UNKNOWN
---
+++ tests/cfgnet/plugins/concept/test_spring_plugin.py
@@ -0,0 +1,279 @@
+# This file is part of the CfgNet module.
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import os
+
+import pytest
+
+from cfgnet.config_types.config_types import ConfigType
+from cfgnet.plugins.concept.spring_plugin import SpringPlugin
+from tests.utility.id_creator import make_id
+
+
+@pytest.fixture(name="get_plugin")
+def get_plugin_():
+    plugin = SpringPlugin()
+    return plugin
+
+
+def test_is_responsible(get_plugin):
+    spring_plugin = get_plugin
+
+    application_yml_file = spring_plugin.is_responsible(
+        "tests/files/application.yml"
+    )
+    application_yml_dev_file = spring_plugin.is_responsible(
+        "tests/files/application-dev.yml"
+    )
+    application_yml_prod_file = spring_plugin.is_responsible(
+        "tests/files/application.prod.yml"
+    )
+    application_properties_file = spring_plugin.is_responsible(
+        "tests/files/application.properties"
+    )
+    application_properties_dev_file = spring_plugin.is_responsible(
+        "tests/files/application.prod.properties"
+    )
+    application_properties_prod_file = spring_plugin.is_responsible(
+        "tests/files/application-dev.properties"
+    )
+    bootstrap_properties_file = spring_plugin.is_responsible(
+        "tests/files/bootstrap.properties"
+    )
+    bootstrap_yml_file = spring_plugin.is_responsible(
+        "tests/files/bootstrap.yml"
+    )
+    no_spring_file = spring_plugin.is_responsible("tests/files/test.yml")
+
+    assert application_yml_file
+    assert application_yml_prod_file
+    assert application_yml_dev_file
+    assert application_properties_file
+    assert application_properties_dev_file
+    assert application_properties_prod_file
+    assert bootstrap_yml_file
+    assert bootstrap_properties_file
+    assert not no_spring_file
+
+
+def test_parsing_spring_properties_file(get_plugin):
+    spring_plugin = get_plugin
+    file = os.path.abspath("tests/files/application.properties")
+
+    artifact = spring_plugin.parse_file(file, "application.properties")
+    nodes = artifact.get_nodes()
+    ids = {node.id for node in nodes}
+
+    assert artifact is not None
+    assert len(nodes) == 12
+
+    assert (
+        make_id("application.properties", "file", "application.properties")
+        in ids
+    )
+    assert make_id("application.properties", "server.port", "8090") in ids
+    assert (
+        make_id(
+            "application.properties",
+            "spring.datasource.url",
+            "jdbc:h2:mem:blog_simple_db",
+        )
+        in ids
+    )
+    assert (
+        make_id("application.properties", "spring.datasource.username", "sa")
+        in ids
+    )
+    assert (
+        make_id("application.properties", "spring.datasource.password", "abc")
+        in ids
+    )
+    assert (
+        make_id(
+            "application.properties",
+            "spring.datasource.data",
+            "classpath:/sql/import-h2.sql",
+        )
+        in ids
+    )
+    assert (
+        make_id("application.properties", "spring.h2.console.enabled", "true")
+        in ids
+    )
+    assert (
+        make_id(
+            "application.properties", "spring.h2.console.path", "/h2-console"
+        )
+        in ids
+    )
+    assert (
+        make_id("application.properties", "spring.admin.password", "admin")
+        in ids
+    )
+    assert (
+        make_id("application.properties", "spring.admin.password", "admin")
+        in ids
+    )
+    assert (
+        make_id("application.properties", "spring.thymeleaf.cache", "false")
+        in ids
+    )
+    assert (
+        make_id(
+            "application.properties",
+            "spring.thymeleaf.prefix",
+            "classpath:/templates",
+        )
+        in ids
+    )
+
+
+def test_parsing_spring_yml_file(get_plugin):
+    spring_plugin = get_plugin
+    file = os.path.abspath("tests/files/application.yml")
+
+    artifact = spring_plugin.parse_file(file, "application.yml")
+    nodes = artifact.get_nodes()
+    ids = {node.id for node in nodes}
+
+    assert artifact is not None
+    assert len(nodes) == 14
+
+    assert make_id("application.yml", "file", "application.yml") in ids
+    assert (
+        make_id(
+            "application.yml",
+            "spring.datasource.url",
+            "http://localhost:3306/",
+        )
+        in ids
+    )
+    assert (
+        make_id(
+            "application.yml", "spring.datasource.driver-class-name", "driver"
+        )
+        in ids
+    )
+    assert (
+        make_id("application.yml", "spring.datasource.username", "dev") in ids
+    )
+    assert (
+        make_id("application.yml", "spring.datasource.password", "dev") in ids
+    )
+    assert (
+        make_id("application.yml", "spring.datasource.platform", "mysql")
+        in ids
+    )
+    assert (
+        make_id("application.yml", "spring.jpa.hibernate.ddl-auto", "validate")
+        in ids
+    )
+    assert (
+        make_id(
+            "application.yml",
+            "spring.jpa.database-platform",
+            "org.hibernate.dialect",
+        )
+        in ids
+    )
+    assert make_id("application.yml", "spring.jpa.database", "mysql") in ids
+    assert make_id("application.yml", "spring.jpa.show-sql", "True") in ids
+    assert (
+        make_id("application.yml", "default.admin.image", "admin.png") in ids
+    )
+    assert (
+        make_id("application.yml", "default.admin.mail", "admin@mail.com")
+        in ids
+    )
+    assert make_id("application.yml", "default.admin.name", "Admin") in ids
+    assert make_id("application.yml", "default.admin.password", "1234") in ids
+
+
+def yml_config_types(get_plugin):
+    spring_plugin = get_plugin
+    file = os.path.abspath("tests/files/application.yml")
+
+    artifact = spring_plugin.parse_file(file, "application.yml")
+    nodes = artifact.get_nodes()
+
+    url_node = next(
+        filter(
+            lambda x: x.id
+            == make_id(
+                "application.yml",
+                "spring.datasource.url",
+                "http://localhost:3306/",
+            ),
+            nodes,
+        )
+    )
+    username_node = next(
+        filter(
+            lambda x: x.id
+            == make_id("application.yml", "spring.datasource.username", "dev"),
+            nodes,
+        )
+    )
+    password_node = next(
+        filter(
+            lambda x: x.id
+            == make_id("application.yml", "spring.datasource.password", "dev"),
+            nodes,
+        )
+    )
+
+    assert url_node.config_type == ConfigType.URL
+    assert username_node.config_type == ConfigType.USERNAME
+    assert password_node.config_type == ConfigType.PASSWORD
+
+
+def properties_config_types(get_plugin):
+    spring_plugin = get_plugin
+    file = os.path.abspath("tests/files/application.properties")
+
+    artifact = spring_plugin.parse_file(file, "application.properties")
+    nodes = artifact.get_nodes()
+
+    port_node = next(
+        filter(
+            lambda x: x.id
+            == make_id("application.properties", "server.port", "8090"),
+            nodes,
+        )
+    )
+    path_node = next(
+        filter(
+            lambda x: x.id
+            == make_id(
+                "application.properties",
+                "spring.h2.console.path",
+                "/h2-console",
+            ),
+            nodes,
+        )
+    )
+    boolean_node = next(
+        filter(
+            lambda x: x.id
+            == make_id(
+                "application.properties", "spring.thymeleaf.cache", "false"
+            ),
+            nodes,
+        )
+    )
+
+    assert port_node.config_type == ConfigType.PORT
+    assert path_node.config_type == ConfigType.PATH
+    assert boolean_node.config_type == ConfigType.BOOLEAN
